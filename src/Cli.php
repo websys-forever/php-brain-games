db78c717aa2d6dd38fd6430f9d930771b7bf6d1d
websys.forever <websys.forever@gmail.com>
2020-06-06 17:48:48 +0500

    Added asciinema

--- src/Cli.php
+++ src/Cli.php
@@ -5,27 +5,113 @@ declare(strict_types=1);
 namespace BrainGames\Cli;
 
 use function Cli\{out, prompt};
-use function BrainGames\Games\Even\{getDescription, runGameProcess};
+
+const WIN_ANSWER_COUNT = 3;
+const CORRECT_ANSWER_MESSAGE = "Correct!\n";
 
 /**
  * Run CLI application
  *
+ * @param $gameName games's name
+ *
  * @return void
- * @throws \Exception
  */
-function run()
+function run($gameName)
 {
     try {
+        $getDescription = "BrainGames\Games\\{$gameName}\getDescription";
+        $getQuestionValues = "BrainGames\Games\\{$gameName}\getQuestionValues";
+        $getRightAnswer = "BrainGames\Games\\{$gameName}\getRightAnswer";
+
         out("Welcome to the Brain Games!\n");
-        out(getDescription());
+        out($getDescription());
 
         $userName = prompt("May I have your name?");
         out("Hello, {$userName}\n");
 
-        $result = runGameProcess($userName);
+        $result = processGame($userName, $getQuestionValues, $getRightAnswer);
         out($result);
     } catch (\Throwable $e) {
         out("Error: {$e->getMessage()}\n");
         return $e->getCode();
     }
+}
+
+/**
+ * Process game
+ *
+ * @param $userName          string user's name
+ * @param $getQuestionValues string game's function which return question
+ * @param $getRightAnswer    string game's function which return right answer
+ *
+ * @return string
+ */
+function processGame($userName, $getQuestionValues, $getRightAnswer)
+{
+    $rightAnswersCount = 0;
+    while ($rightAnswersCount < WIN_ANSWER_COUNT) {
+        $questionValues = $getQuestionValues();
+        $rightAnswer = $getRightAnswer($questionValues);
+        $questionString = getQuestionString($questionValues);
+
+        $receivedAnswer = prompt("Question: {$questionString}");
+
+        if ($rightAnswer == $receivedAnswer) {
+            $rightAnswersCount += 1;
+            out(CORRECT_ANSWER_MESSAGE);
+        } else {
+            return getWrongAnswerMessage($userName, $rightAnswer, $receivedAnswer);
+        }
+    }
+
+    return getWinMessage($userName);
+}
+
+/**
+ * Get wrong answer message
+ *
+ * @param $userName       string user's name
+ * @param $needAnswer     string correct answer
+ * @param $receivedAnswer string user's answer
+ *
+ * @return string
+ */
+function getWrongAnswerMessage($userName, $needAnswer, $receivedAnswer): string
+{
+    $message = <<<MESSAGE
+'{$receivedAnswer}' is wrong answer ;(. Correct answer was '{$needAnswer}'.
+Let's try again, {$userName}!\n
+MESSAGE;
+
+    return $message;
+}
+
+/**
+ * Get game win message
+ *
+ * @param $userName string user's name
+ *
+ * @return string
+ */
+function getWinMessage($userName): string
+{
+    $message = <<<MESSAGE
+Congratulations, {$userName}!\n
+MESSAGE;
+
+    return $message;
+}
+
+/**
+ * Get question string
+ *
+ * @param $questionValues mixed question values
+ *
+ * @return string
+ */
+function getQuestionString($questionValues): string
+{
+    return is_array($questionValues) ?
+        implode(' ', $questionValues)
+        : (string) $questionValues;
 }
---
+++ src/Games/Calc.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BrainGames\Games\Calc;
+
+const GAME_DESCRIPTION = <<<MESSAGE
+What is the result of the expression?\n
+MESSAGE;
+
+const RAND_MIN_NUMBER = 1;
+const RAND_MAX_NUMBER = 100;
+const OPERATIONS = ['+', '-', '*'];
+
+/**
+ * Get game's description
+ *
+ * @return string
+ */
+function getDescription(): string
+{
+    return GAME_DESCRIPTION;
+}
+
+
+/**
+ * Get game's question
+ *
+ * @return array
+ */
+function getQuestionValues(): array
+{
+    $num1 = rand(RAND_MIN_NUMBER, RAND_MAX_NUMBER);
+    $num2 = rand(RAND_MIN_NUMBER, RAND_MAX_NUMBER);
+    $randKey = array_rand(OPERATIONS);
+    $operation = OPERATIONS[$randKey];
+
+    return [
+        'num1' => $num1,
+        'operation' => $operation,
+        'num2' => $num2
+    ];
+}
+
+/**
+ * Get game's right answer
+ *
+ * @param array $questionValues question's values
+ *
+ * @return int
+ */
+function getRightAnswer(array $questionValues): int
+{
+    switch ($questionValues['operation']) {
+        case '+':
+            $result = $questionValues['num1'] + $questionValues['num2'];
+
+            break;
+        case '-':
+            $result = $questionValues['num1'] - $questionValues['num2'];
+
+            break;
+        case '*':
+            $result = $questionValues['num1'] * $questionValues['num2'];
+
+            break;
+    }
+
+    return $result;
+}
