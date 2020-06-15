b7ee6080427ec75c43e5ad365047e53bde76adf0
websys.forever <websys.forever@gmail.com>
2020-06-15 20:12:07 +0500

    Fix psr style

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace BrainGames\Games\Calc;
 
-use const BrainGames\Common\{RAND_MIN_NUMBER, RAND_MAX_NUMBER};
-
 use function BrainGames\Cli\processGameFlow;
+
+use const BrainGames\Common\{RAND_MIN_NUMBER, RAND_MAX_NUMBER};
 
 const GAME_DESCRIPTION = <<<MESSAGE
 What is the result of the expression?\n
